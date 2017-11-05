dc7df74d61bc6efe7f1103f4a4aecc2056cf0d02
Jenny Lian <jennylian100@gmail.com>
2017-11-04 21:33:27 -0700

    joke Api implemented

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var request = require('request'); // reference for simple GET request
 
 app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+request('https://www.reddit.com/r/Jokes/top.json?limit=1', function(error, response, body){
+    var resp = JSON.parse(body);
+    var jokeTitle = resp.data.children[0].data.title;
+    var jokeText = resp.data.children[0].data.selftext;
+	res.send({
+    jokeTitle: jokeText
+  });})
+  
 });
 
 app.post('/', function(req, res) {
